47564990988fcf296463cd065c57e4b6560ed059
Daniel <danieldeandradelopes@gmail.com>
2023-07-20 15:24:30 -0300

    feat: add usecases and repositorys patterns

--- src/Pokemons/repositories/databaseRepository.js
+++ src/Pokemons/repositories/databaseRepository.js
@@ -1,15 +1,4 @@
-const { Pool } = require("pg");
-
-const pool = new Pool({
-  host: "isilo.db.elephantsql.com",
-  port: 5432,
-  database: "unpxcmny",
-  user: "unpxcmny",
-  password: "P7KEdEKJY350JhgZcEBqSzYo9cSQsyKG",
-  max: 20,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
-});
+const pool = require("../../configs/database");
 
 async function getAll() {
   const res = await pool.query("SELECT * from pokemons");
