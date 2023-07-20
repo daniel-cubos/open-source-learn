640794f8b039fb4cabbbd5f10d5660bdcd53aa31
Daniel <danieldeandradelopes@gmail.com>
2023-07-20 16:07:54 -0300

    hotfix

--- src/Pokemons/useCases/listPokemon.spec.js
+++ src/Pokemons/useCases/listPokemon.spec.js
@@ -16,14 +16,4 @@ describe("Test listPokemon useCase", () => {
 
     expect(pikachu.name).toBe("Pikachu");
   });
-
-  test("Should be have pikachu 2 in list", async () => {
-    const insertedPokemon = await insertPokemon(inMemoryRepository, {
-      name: "Pikachu 2",
-      image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png",
-      type: "Electric",
-    });
-
-    expect(insertedPokemon.name).toBe("Pikachu 2");
-  });
 });
